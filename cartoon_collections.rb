c4d033ccbddccc9e5785023cb09ff0fc6aaf3a60
Jacob Viti <thevitij64@gmail.com>
2020-07-14 23:14:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,9 @@ def roll_call_dwarves(array)
 puts currentlyLine
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|element| element + "!"}
+  puts array
 end
 
 def long_planeteer_calls# code an argument here
