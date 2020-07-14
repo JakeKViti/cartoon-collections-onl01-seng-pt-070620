3f17f0c08979665840817212ee54846ec5123d0a
Jacob Viti <thevitij64@gmail.com>
2020-07-14 23:35:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,11 @@ def summon_captain_planet(array)
 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length < 5
+    return true
+  else 
+    return false
 end
 
 def find_the_cheese# code an argument here
